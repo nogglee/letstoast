ca5f88d31e60837ea9d06966bfdda2f89612b888
이은지 <eunjilee@Mac-mini.local>
2024-11-07 11:00:27 +0900

    FIX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { Halmet }from 'react-helaet';
+import { Halmet }from 'react-helmet';
 import Home from './pages/Home'; 
 import Timer from './pages/Timer';
 import Result from './pages/Result';
